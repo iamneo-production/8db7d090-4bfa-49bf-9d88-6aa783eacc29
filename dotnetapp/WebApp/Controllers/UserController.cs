26ad73bcd0a61723b2da42c0b3c073b3b130331e
Muralikrishnangopi <113250664+Muralikrishnangopi@users.noreply.github.com>
2023-07-28 19:33:37 +0530

    User

---
+++ dotnetapp/WebApp/Controllers/UserController.cs
@@ -0,0 +1,126 @@
+using Aloans.Data;
+using Aloans.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace User.Controller
+{
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly AloansDbContext _context;
+        public UserController(AloansDbContext applicationdbcontext)
+        {
+            _context = applicationdbcontext;
+        }
+        [HttpPost("user/addProfile")]
+        public async Task<IActionResult> addUser([FromBody] UserModel userobj)
+        {
+            if (userobj == null)
+            {
+                return BadRequest();
+            }
+
+            await _context.User.AddAsync(userobj);
+            await _context.SaveChangesAsync();
+
+            return Ok(new
+            {
+                Message = "User profile created"
+            });
+        }
+
+        [HttpGet("user/GetProfile/{ID}")]
+        public async Task<IActionResult> getProfile(int ID)
+        {
+            var Userprofile = await _context.User.FirstOrDefaultAsync(p => p.ID == ID);
+
+            if (Userprofile == null)
+            {
+                return NotFound(new
+                {
+                    Message = "No user found"
+                });
+            }
+
+            return Ok(Userprofile);
+
+
+        }
+        [HttpGet("user/getProfile")]
+        public async Task<IActionResult> getProfiles()
+        {
+            var Userprofile = await _context.User.ToListAsync();
+
+            if (Userprofile == null || Userprofile.Count === 0)
+            {
+                return NotFound(new
+                {
+                    Message = "No user found"
+                });
+            }
+
+            return Ok(new
+            {
+                Message = "View Profile",
+                UserDetails = Userprofile
+            });
+
+        }
+        [HttpDelete("user/deleteProfile/{ID}")]
+        public async Task<IActionResult> deleteUser(int ID)
+        {
+            var Userprofile = await _context.User.FindAsync(ID);
+
+            if (Userprofile == null)
+            {
+                return NotFound();
+            }
+
+            _context.User.Remove(Userprofile);
+            await _context.SaveChangesAsync();
+
+
+            return Ok(new
+            {
+                Message = "Delete profile",
+                UserDetails = Userprofile
+            });
+        }
+        [HttpPut("user/editProfile/{ID}")]
+        public async Task<IActionResult> EditUserProfile(int ID, [FromBody] UserModel userModel)
+        {
+            if (userModel == null || ID != userModel.ID)
+            {
+                return BadRequest();
+            }
+
+            var Userprofile = await _context.User.FindAsync(ID);
+
+            if (Userprofile == null)
+            {
+                return NotFound();
+            }
+
+            Userprofile.email = userModel.email;
+            Userprofile.password = userModel.password;
+            Userprofile.userName = userModel.userName;
+            Userprofile.confirmpassword = userModel.confirmpassword;
+            Userprofile.mobileNumber = userModel.mobileNumber;
+            Userprofile.userRole = userModel.userRole;
+
+
+            _context.User.Update(Userprofile);
+            await _context.SaveChangesAsync();
+
+            return Ok(new
+            {
+                Message = "Updated profile",
+                UserDetails = Userprofile
+            });
+        }
+    }
+}
